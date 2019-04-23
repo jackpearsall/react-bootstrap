78ae7af30e973e9610302afbb95fc577a2594477
Jack Pearsall <jack.pearsall@visiosound.co.uk>
2019-04-23 23:51:09 +0100

    Styling improvments - done!

--- src/components/search-form.jsx
+++ src/components/search-form.jsx
@@ -4,7 +4,7 @@ const SearchForm = props => (
   <div className="searchBox">
     <input type="text" value={props.searchText} placeholder="Enter Location...." onKeyPress={props.handleKeyDown} onChange={props.handleInputChange} />
     <button onClick={() => props.updateCity(props.searchText)} type="submit">Search</button>
-    <span className="error">{props.errorMessage}</span>
+    <span><div className="error">{props.errorMessage}</div></span>
   </div>
 );
 
